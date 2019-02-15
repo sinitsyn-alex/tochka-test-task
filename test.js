457dcaa3fb67505232d6d21d4e002c070b57c12b
Sinitsyn Alexandr <sinitsyn.aleks@gmail.com>
2019-02-15 20:43:30 +0500

    init

---
+++ test.js
@@ -0,0 +1,50 @@
+const swap = (str) => {
+  let result = ''
+
+  for (let idx = 0; idx < str.length; idx++) {
+    if ((idx + 1) % 2 === 0) {
+      result += str[idx] + str[idx - 1]
+    }
+  }
+  str.length % 2 && (result += str[str.length - 1])
+  return result
+}
+
+// Tests
+console.log('Test swap:', swap('apple') === 'palpe')
+console.log('Test swap:', swap('lemonade') === 'elomaned')
+
+const swapReduce = (str) => {
+  let result = [...str].reduce((acc, current, idx) => {
+    if ((idx + 1) % 2 === 0) {
+      acc += current + str[idx - 1]
+    }
+    return acc
+  }, '')
+  str.length % 2 && (result += str[str.length - 1])
+  return result
+}
+
+// Tests
+console.log('Test swapReduce:', swapReduce('apple') === 'palpe')
+console.log('Test swapReduce:', swapReduce('lemonade') === 'elomaned')
+
+
+const swapRecursive = (str) => {
+  return function wrap (idx = 1, result = '') {
+    // Условие завершения рекурсии
+    if (str.length !== idx) {
+      if ((idx + 1) % 2 === 0) {
+        result += str[idx] + str[idx - 1]
+      }
+      return wrap(idx + 1, result)
+    } else {
+      str.length % 2 && (result += str[str.length - 1])
+      return result
+    }
+  }()
+}
+
+// Tests
+console.log('Test swapRecursive:', swapRecursive('apple') === 'palpe')
+console.log('Test swapRecursive:', swapRecursive('lemonade') === 'elomaned')
